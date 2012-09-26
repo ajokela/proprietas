9928a475076d7d3ec005f52b50bb0b6ff4c3c2ce
Alex Jokela <alex@camulus.com>
2012-09-25 22:40:30 -0500

    more tinkering

--- db/migrate/20120924201500_create_extensions.rb
+++
@@ -1,6 +0,0 @@
-class CreateStates < ActiveRecord::Migration
-  def change
-    execute("CREATE EXTENSION postgis")
-    execute("CREATE EXTENSION postgis_topology")
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(:version => 20120924201607) do
   end
 
   create_table "states", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "name",       :limit => 128
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
 end
