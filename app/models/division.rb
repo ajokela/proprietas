0dc8656ea9f6515a11695505ede57e2b6382ac87
Alex Jokela <alex@camulus.com>
2012-10-02 22:19:22 -0500

    adding belongs_to relationships to models

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,3 +1,4 @@
 class Division < ActiveRecord::Base
   # attr_accessible :title, :body
+  belongs_to :region
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,3 +1,4 @@
 class Region < ActiveRecord::Base
   # attr_accessible :title, :body
+  belongs_to :country
 end
